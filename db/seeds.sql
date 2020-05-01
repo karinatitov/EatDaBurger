5ded95a0ac619ba1b0b699d3486e74ad85187065
Karina Titov <karinasul1995@gmail.com>
2020-05-01 03:08:13 -0500

    tired creating all these files :D

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name) VALUES ("Mushroom Swissburger");
+INSERT INTO burger (burger_name, devoured) VALUES ("Big Mac", true);
+INSERT INTO burger (burger_name) VALUES ("Chicken Sandwich");
